10409fb48eadb973474a78db6e26921027272976
sl-badcoder <senadlg01@gmail.com>
2024-08-15 20:03:30 +0200

    second

--- main.py
+++ main.py
@@ -1,1 +1,66 @@
-print("Hello World")
+import requests
+import re
+import urllib.request
+from bs4 import BeautifulSoup
+from collections import deque
+from html.parser import HTMLParser
+from urllib.parse import urlparse
+import os
+
+# Regex pattern to match a URL
+HTTP_URL_PATTERN = r'^http[s]*://.+'
+
+domain = "openai.com" # <- put your domain to be crawled
+full_url = "https://openai.com/" # <- put your domain to be crawled with https or http
+
+def crawl(url):
+    # Parse the URL and get the domain
+    local_domain = urlparse(url).netloc
+
+    # Create a queue to store the URLs to crawl
+    queue = deque([url])
+
+    # Create a set to store the URLs that have already been seen (no duplicates)
+    seen = set([url])
+
+    # Create a directory to store the text files
+    if not os.path.exists("text/"):
+            os.mkdir("text/")
+
+    if not os.path.exists("text/"+local_domain+"/"):
+            os.mkdir("text/" + local_domain + "/")
+
+    # Create a directory to store the csv files
+    if not os.path.exists("processed"):
+            os.mkdir("processed")
+
+    # While the queue is not empty, continue crawling
+    while queue:
+
+        # Get the next URL from the queue
+        url = queue.pop()
+        print(url) # for debugging and to see the progress
+
+        # Save text from the url to a <url>.txt file
+        with open('text/'+local_domain+'/'+url[8:].replace("/", "_") + ".txt", "w", encoding="UTF-8") as f:
+
+            # Get the text from the URL using BeautifulSoup
+            soup = BeautifulSoup(requests.get(url).text, "html.parser")
+
+            # Get the text but remove the tags
+            text = soup.get_text()
+
+            # If the crawler gets to a page that requires JavaScript, it will stop the crawl
+            if ("You need to enable JavaScript to run this app." in text):
+                print("Unable to parse page " + url + " due to JavaScript being required")
+
+            # Otherwise, write the text to the file in the text directory
+            f.write(text)
+
+        # Get the hyperlinks from the URL and add them to the queue
+        for link in get_domain_hyperlinks(local_domain, url):
+            if link not in seen:
+                queue.append(link)
+                seen.add(link)
+
+crawl(full_url)
